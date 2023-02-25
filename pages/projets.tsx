4aa5d87dc580d5467d0bc8a6af143478f4051a11
hugues leger <hugueslegerdevweb@gmail.com>
2023-02-25 13:55:35 +0100

    add page projets

---
+++ pages/projets.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NextPage } from "next";
+
+const ProjetPage: NextPage = (props) => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default ProjetPage;
