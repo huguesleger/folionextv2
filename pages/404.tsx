a6c35d6ac654665ab4a24c6c65b601aa337a984b
hugues leger <hugueslegerdevweb@gmail.com>
2023-03-08 12:08:51 +0100

    finish style page error 404 + add infiniteKeywords

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,68 @@
+import Link from "next/link";
 import React from "react";
+import ChangeWord from "../components/ChangeWord";
+import Footer from "../components/layout/Footer";
 
 export default function Custom404() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div className="page-error">
+      <div className="wrapper">
+        <div className="container">
+          <div className="wrap-content">
+            <div className="error-infos">
+              <ChangeWord
+                word1={"Oups !"}
+                word2={"C'est la mouise !"}
+                word3={"Mince !"}
+                word4={"F*** !"}
+              />
+              <p>une erreur est survenue</p>
+            </div>
+
+            <div className="infinite-keywords">
+              <p className="keywords">
+                <span className="wrap-keyword keyword-bold">
+                  404
+                  <span className="separate-keyword">
+                    <i className="fas fa-circle" aria-hidden></i>
+                  </span>
+                </span>
+                <span className="wrap-keyword">
+                  Cette page n&apos;existe pas
+                  <span className="separate-keyword">
+                    <i className="fas fa-circle" aria-hidden></i>
+                  </span>
+                </span>
+              </p>
+              <p className="keywords">
+                <span className="wrap-keyword keyword-bold">
+                  404
+                  <span className="separate-keyword">
+                    <i className="fas fa-circle" aria-hidden></i>
+                  </span>
+                </span>
+                <span className="wrap-keyword">
+                  Cette page n&apos;existe pas
+                  <span className="separate-keyword">
+                    <i className="fas fa-circle" aria-hidden></i>
+                  </span>
+                </span>
+              </p>
+            </div>
+            <div className="wrap-btn">
+              <Link href="/" className="btn btn-xl btn-dark btn-effect">
+                <span>
+                  <span>
+                    <i className="fas fa-home" aria-hidden></i>
+                    Retour à l&apos;accueil
+                  </span>
+                </span>
+              </Link>
+            </div>
+            <Footer type={"footer-dark"} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
