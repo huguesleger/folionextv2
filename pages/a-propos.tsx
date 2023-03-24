6a723f64f28478e45927ec698a0847a615768349
hugues leger <hugueslegerdevweb@gmail.com>
2023-03-24 22:46:41 +0100

    remove dataSpeed section cursus page about

--- pages/a-propos.tsx
+++ pages/a-propos.tsx
@@ -150,7 +150,7 @@ const APropos: (props: { about: GraphQLResponse.About }) => JSX.Element =
                   {props.about.titreCursus}
                 </h2>
               </div>
-              <div className="list-cursus" data-scroll data-scroll-speed="3">
+              <div className="list-cursus" data-scroll>
                 {props.about.listeCursus.map((el) => {
                   return (
                     <div className="wrap-list-item" key={el.id}>
