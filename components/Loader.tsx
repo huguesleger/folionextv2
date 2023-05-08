f066b5f116b11c670e8b5ecd7d2750b22d058bd5
hugues leger <hugueslegerdevweb@gmail.com>
2023-05-08 12:28:03 +0200

    add loader in localStorage

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,24 +3,33 @@ import ChangeWord from "./ChangeWord";
 import gsap from "gsap";
 
 const Loader = () => {
-  const loader = useRef(null);
+  const loader = useRef<HTMLDivElement>(null);
+  let date = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
 
   useEffect(() => {
     const intro = document.querySelector(".intro");
     const tl = gsap.timeline();
-    tl.to(loader.current, {
-      delay: 7,
-      opacity: 0,
-      pointerEvents: "none",
-      ease: "Power2.easeInOut",
-      duration: 0.5,
-      visibility: "hidden",
-      onComplete: () => {
-        if (intro) {
-          intro.classList.add("is-show");
-        }
-      },
-    });
+
+    if (localStorage.getItem("preloader") == date.toString()) {
+      loader.current?.remove();
+    }
+
+    if (localStorage.getItem("preloader") != date.toString()) {
+      tl.to(loader.current, {
+        delay: 7,
+        opacity: 0,
+        pointerEvents: "none",
+        ease: "Power2.easeInOut",
+        duration: 0.5,
+        visibility: "hidden",
+        onComplete: () => {
+          if (intro) {
+            intro.classList.add("is-show");
+          }
+        },
+      });
+      localStorage.setItem("preloader", date.toString());
+    }
   }, []);
 
   return (
